dc679a934a625cfc3170874635ec88a609f8908d
DavidEsdrs <carpina1983@gmail.com>
2023-05-27 04:46:58 -0300

    feat: Create multer fileFilter and fileName 'interceptors'

---
+++ src/config/multer.config.ts
@@ -0,0 +1,16 @@
+import { FileFilterCallback } from "multer";
+import { extname } from "path";
+import { Request } from "express";
+
+export const imageFileFilter = (req: Request, file: Express.Multer.File, callback: FileFilterCallback) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+      return callback(null, false);
+    }
+    callback(null, true);
+};
+
+export const editFileName = (req: Request, file: Express.Multer.File, callback: (...args: any) => any) => {
+    const fileExtName = extname(file.originalname);
+    const name = Date.now();
+    callback(null, `${name}${fileExtName}`);
+};
