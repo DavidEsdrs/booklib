7d0a260b39891be1b08cd97a86a20810fa5ab03b
DavidEsdrs <carpina1983@gmail.com>
2023-05-28 07:21:35 -0300

    feat: Bind ValidationPipe and ApiExceptionFilter to the app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, UnprocessableEntityException, ValidationError, ValidationPipe } from "@nestjs/common";
+import { ApiExceptionFilter } from "./lib/errorHandler.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    exceptionFactory: (errors: ValidationError[]) => {
+      const errorMessage = errors.map(error => Object.values(error.constraints)).join(', ');
+      return new UnprocessableEntityException(errorMessage);
+    }
+  }));
+  app.useGlobalFilters(new ApiExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
