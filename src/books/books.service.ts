652e0e3c0d3e07d21ac3747db71a7112891c9656
DavidEsdrs <carpina1983@gmail.com>
2023-05-28 11:21:00 -0300

    feat: Save book and its genders in database

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -14,8 +14,24 @@ export class BooksService {
         pages, 
         synopsis, 
         visibility, 
-        genders 
-    }: BookDTO) {
-        return "funfou"
+        genders,
+        uploadedById
+    }: BookDTO & { uploadedById: number, coverFilePath: string, filePath: string }) {
+        const book = await this.prisma.book.create({
+            data: {
+                title,
+                coverFilePath,
+                filePath,
+                pages,
+                synopsis,
+                notes,
+                genders: {
+                    create: genders.map(gender => ({ name: gender }))
+                },
+                uploadedById,
+                visibility
+            }
+        });
+        return book;
     }
 }
