be7d896e6e8bd07a94c9355b4b44d12bc7f5ab05
DavidEsdrs <carpina1983@gmail.com>
2023-05-30 18:37:51 -0300

    feat: Import LibraryModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from "./auth/auth.module";
 import { FileSystemModule } from './file-system/file-system.module';
 import { BooksModule } from './books/books.module';
+import { LibraryModule } from './bookshelf/library.module';
 
 @Module({
   imports: [
@@ -11,7 +12,8 @@ import { BooksModule } from './books/books.module';
     UsersModule, 
     AuthModule, 
     FileSystemModule, 
-    BooksModule
+    BooksModule, 
+    LibraryModule
   ],
   controllers: [],
   providers: []
