d410999fceec80a107031927df7b266a8e71f299
DavidEsdrs <carpina1983@gmail.com>
2023-05-30 15:49:16 -0300

    feat: add FileSystemModule to imports

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -3,10 +3,11 @@ import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
 import { UsersModule } from "src/users/users.module";
 import { PrismaModule } from "src/prisma/prisma.module";
+import { FileSystemModule } from "src/file-system/file-system.module";
 
 @Module({
   controllers: [BooksController],
   providers: [BooksService],
-  imports: [UsersModule, PrismaModule]
+  imports: [UsersModule, PrismaModule, FileSystemModule]
 })
 export class BooksModule {}
